84f8ab21ae2d7b25512f9b1be2904e10e21a160a
Olav Landsverk <olav.landsverk@gmail.com>
2026-01-20 09:26:22 +0100

    extension starter
    Signed-off-by: Olav Landsverk <olav.landsverk@gmail.com>

---
+++ _extensions/olav-test/olav-test.lua
@@ -0,0 +1,7 @@
+return {
+  ['olav-test'] = function(args, kwargs, meta, raw_args, context) 
+    -- see https://quarto.org/docs/extensions/shortcodes.html
+    -- for documentation on shortcode development
+    return pandoc.Str("Hello from Olav-test!")
+  end
+}
